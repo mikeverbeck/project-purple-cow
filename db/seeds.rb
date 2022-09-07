6e3a2e22978b317f393615d88ddb76cfed46b1a2
Mike Verbeck <mikeverbeck@gmail.com>
2022-09-07 14:03:00 -0400

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Item.create(name: 'Beach Ball')
+Item.create(name: 'Blanket')
+Item.create(name: 'Cooler')
+Item.create(name: 'Sunglasses')
+Item.create(name: 'Umbrella')
+Item.create(name: 'Surfboard')
+Item.create(name: 'Beach Chair')
+Item.create(name: 'Suntan lotion')
+Item.create(name: 'Hat')
